bbe95d76f6257751929617849e3db1329c804ffc
masonjbaughan <masonjbaughan@gmail.com>
2022-04-30 22:13:47 -0400

    it is my child, my sweet sweet child

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -3,7 +3,7 @@ import { TouchableOpacity, Text } from "react-native";
 
 export function CustomButton(props) {
     return (
-        <TouchableOpacity onPress={props.onPress} color='#0053f0' style={{ justifyContent: 'center', alignItems: 'center', borderRadius: 10, overflow: 'hidden', backgroundColor: '#0053f0', height: 50}}>
+        <TouchableOpacity onPress={props.onPress} color='#0053f0' style={{ justifyContent: 'center', alignItems: 'center', borderRadius: 10, overflow: 'hidden', backgroundColor: '#0053f0', height: 50, width: props.width}}>
             <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'white' }}>{props.title}</Text>
         </TouchableOpacity>
     )
